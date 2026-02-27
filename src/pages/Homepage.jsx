ceb085ef4a05fc29d21c386a068689a611a603d7
Pietro Scurti <pietroscurti9@gmail.com>
2026-02-27 17:18:01 +0100

    fix pages

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+
+import travels_db from "../data/travels_db"
+
 function HomePage() {
-return(
-    <h1>sono la homepage</h1>
-)
-  
+
+    return (
+        <section>
+            <h2>Lista viaggi</h2>
+            <div className="travel-list">
+                <div className="wrap-travel">
+                    <div className="single-travel"></div>
+                </div>
+            </div>
+
+        </section>
+    )
+
 }
+
 export default HomePage
