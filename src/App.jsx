9ceccf10fffcb39d2f8a98dc7e0ad2b678b8c7fd
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-27 16:57:53 +0100

    set-up rotte di base

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import MainHeader from './components/MainHeader';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+//imort tutti i componenti di gestione delle rotte
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//import layout
+import DefaultLayout from "./layouts/DefaultLayout";
+
+//import pages
+import HomePage from "./pages/Homepage";
+
+import MainHeader from "./components/MainHeader";
 
 function App() {
-
   return (
-    <>
-    <MainHeader />
-    </>
-    // <h1 className='text-primary d-flex justify-content-center'>sono app.jsx</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/travels/:id" element={<TravelPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
