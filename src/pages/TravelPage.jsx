e9eb76fea6ba0b03b68a227f171c2b2c6568c7da
Alessandro Ledda <aleledda75@icloud.com>
2026-02-28 09:11:39 +0100

    fix

--- src/pages/TravelPage.jsx
+++ src/pages/TravelPage.jsx
@@ -1,9 +1,9 @@
 import travels from "../data/travels_db"
 
 function TravelPage() {
-return(
-    <h1>sono la travelpage</h1>
-)
-  
+    return (
+        <h1>sono la travelpage</h1>
+    )
+
 }
 export default TravelPage
