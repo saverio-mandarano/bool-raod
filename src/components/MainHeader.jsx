d4abdeec11cfd05222236ae9de4c51af1a4e4034
Pietro Scurti <pietroscurti9@gmail.com>
2026-02-28 19:26:10 +0100

    aggiunto nuovo logo, classi css per header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import headerImg from "../assets/pngegg.png"
 
 function MainHeader() {
     return (
@@ -8,10 +9,11 @@ function MainHeader() {
 
                 <div className="container my-3">
 
-                    <div className="d-flex justify-content-between">
-                        {/* <span className="header-logo">Logo</span> */}
-                        <img src="https://e7.pngegg.com/pngimages/318/944/png-clipart-logo-travel-agent-train-airline-ticket-travel-text-logo-thumbnail.png" alt="" />
-{/* AGGIUNTO LOGO DA RIDIMENSIONARE IN CSS */}
+                    <div className="d-flex justify-content-between header-settings">
+                        <div>
+                            <img src={headerImg} alt="" className="header-logo" />
+                        </div>
+                        {/* AGGIUNTO LOGO DA RIDIMENSIONARE IN CSS */}
                         <Link to={`/`}>
                             <button className="btn btn-primary"> Homepage </button>
                         </Link>
