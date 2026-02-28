15bf868db0e72561301b6cf4cff54ac76e09b2c4
Alessandro Ledda <aleledda75@icloud.com>
2026-02-28 09:58:43 +0100

    feat searchBar in file TravelPage.jsx

--- src/pages/TravelPage.jsx
+++ src/pages/TravelPage.jsx
@@ -2,7 +2,14 @@ import travels from "../data/travels_db"
 
 function TravelPage() {
     return (
-        <h1>sono la travelpage</h1>
+        <>
+            <h1>sono la travelpage</h1>
+            {/* search bar per filtraggio turisti */}
+            <div class="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Nome turista" aria-label="Cerca turista" aria-describedby="button-addon2" />
+                <button class="btn btn-primary" type="button" id="button-addon2">Cerca</button>
+            </div>
+        </>
     )
 
 }
