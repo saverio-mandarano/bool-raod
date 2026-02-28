12baf4c950225882ceb815c1c0140246000d80b4
Alessandro Ledda <aleledda75@icloud.com>
2026-02-28 09:02:34 +0100

    fix in file TravelCard.jsx

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 // funzione per settaggio single card per homePage
 function TravelCard(props) {
@@ -16,6 +17,9 @@ function TravelCard(props) {
                 <h5 className="card-title">{localita}</h5>
                 <p className="card-text">{descrizione}</p>
             </div>
+            <Link to={`travels/${id}`}>
+                <button className="btn btn-primary">Dettagli Viaggio</button>
+            </Link>
         </>
     )
 }
