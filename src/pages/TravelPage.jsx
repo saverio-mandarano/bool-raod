8ac2445e4c2a3bb87885eee43dd3c71ff632f0e8
Alessandro Ledda <aleledda75@icloud.com>
2026-02-28 10:00:02 +0100

    fix in file TravelPage.jsx

--- src/pages/TravelPage.jsx
+++ src/pages/TravelPage.jsx
@@ -4,7 +4,7 @@ function TravelPage() {
     return (
         <>
             <h1>sono la travelpage</h1>
-            {/* search bar per filtraggio turisti */}
+            {/* search bar per filtraggio turista */}
             <div class="input-group mb-3">
                 <input type="text" className="form-control" placeholder="Nome turista" aria-label="Cerca turista" aria-describedby="button-addon2" />
                 <button class="btn btn-primary" type="button" id="button-addon2">Cerca</button>
