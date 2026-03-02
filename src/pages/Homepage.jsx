bfcc3332778d3af91345931151c81974c098ad83
saverio mandarano <saverio.mandarano1@gmail.com>
2026-03-02 11:09:13 +0100

    responsive card in homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,13 +6,14 @@ function HomePage() {
   return (
     <>
       <h1 className="mt-3 container">Lista viaggi attivi</h1>
-      <div className="card-container d-flex flex-wrap justify-content-center">
-        {travels_db.map((travel) => (
-          //   <CardViaggio key={travel.id} travelProp={travel} />
-          <div key={travel.id} className="card m-2 w-25">
-            <TravelCard travelProp={travel} />
-          </div>
-        ))}
+      <div className="container ">
+        <div className="row g-4">
+          {travels_db.map((travel) => (
+            <div key={travel.id} className="card col-12 col-md-6 col-lg-4">
+              <TravelCard travelProp={travel} />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
