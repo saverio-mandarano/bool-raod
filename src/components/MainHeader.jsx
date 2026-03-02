da7975dd9b9b83e979442515299c87e94d7252d8
saverio mandarano <saverio.mandarano1@gmail.com>
2026-03-02 10:58:57 +0100

    fix navbar, ora occupa tutta la riga e non ci sono margini superiori

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,32 +1,20 @@
-import { Link } from "react-router-dom"
-import headerImg from "../assets/pngegg.png"
+import { Link } from "react-router-dom";
+import headerImg from "../assets/pngegg.png";
 
 function MainHeader() {
-    return (
-        <>
-            <header>
-
-
-                <div className="container my-3">
-
-                    <div className="d-flex justify-content-between header-settings">
-                        <Link to={`/`}>
-                            <img src={headerImg} alt="" className="header-logo" />
-                        </Link>
-                        {/* AGGIUNTO LOGO DA RIDIMENSIONARE IN CSS */}
-                        <Link to={`/`}>
-                            <button className="btn btn-primary"> Homepage </button>
-                        </Link>
-                    </div>
-
-
-
-                </div>
-
-            </header >
-
-        </>
-    )
+  return (
+    <>
+      <header className=" mb-3 d-flex justify-content-between header-settings">
+        <Link to={`/`}>
+          <img src={headerImg} alt="" className="header-logo" />
+        </Link>
+        {/* AGGIUNTO LOGO DA RIDIMENSIONARE IN CSS */}
+        <Link to={`/`}>
+          <button className="btn btn-primary"> Homepage </button>
+        </Link>
+      </header>
+    </>
+  );
 }
 
-export default MainHeader
+export default MainHeader;
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,15 +1,13 @@
 import { Outlet } from "react-router-dom";
 import MainHeader from "../components/MainHeader";
 
-
 export default function AppLayout() {
-    return (
-        <>
-            <header><MainHeader /></header>
-            <main>
-                <Outlet />
-            </main>
-            
-        </>
-    )
+  return (
+    <>
+      <MainHeader />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
