56366e6cbd22ff126d080f097347c9b83e037f3d
Pietro Scurti <pietroscurti9@gmail.com>
2026-03-02 12:19:33 +0100

    fix header logo

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,19 +2,21 @@ import { Link } from "react-router-dom";
 import headerImg from "../assets/pngegg.png";
 
 function MainHeader() {
-  return (
-    <>
-      <header className=" mb-3 d-flex justify-content-between header-settings">
-        <Link to={`/`}>
-          <img src={headerImg} alt="" className="header-logo" />
-        </Link>
-        {/* AGGIUNTO LOGO DA RIDIMENSIONARE IN CSS */}
-        <Link to={`/`}>
-          <button className="btn btn-primary"> Homepage </button>
-        </Link>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <header className=" mb-3 d-flex justify-content-between header-settings">
+                <div>
+                    <img src={headerImg} alt="" className="header-logo" />
+                </div>
+
+
+
+                <Link to={`/`}>
+                    <button className="btn btn-primary"> Homepage </button>
+                </Link>
+            </header>
+        </>
+    );
 }
 
 export default MainHeader;
