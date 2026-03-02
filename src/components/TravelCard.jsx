9c81acdf6896c8aa1dc17dee5bbfc3e3291d4554
saverio mandarano <saverio.mandarano1@gmail.com>
2026-03-02 11:23:53 +0100

    fix cards in homepage ora hanno tutte la stessa altezza e sono separate da un piccolo gap

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -2,26 +2,21 @@ import { Link } from "react-router-dom";
 
 // funzione per settaggio single card per homePage
 function TravelCard(props) {
+  // destrutturo props
+  const { id, img_url, localita, descrizione } = props.travelProp;
 
-    // destrutturo props
-    const { id, img_url, localita, descrizione } = props.travelProp;
-
-    return (
-        <>
-            <img
-                src={img_url}
-                className="card-img-top"
-                alt={localita}
-            />
-            <div className="card-body">
-                <h5 className="card-title">{localita}</h5>
-                <p className="card-text">{descrizione}</p>
-            </div>
-            <Link to={`travels/${id}`}>
-                <button className="btn btn-primary m-2">Dettagli Viaggio</button>
-            </Link>
-        </>
-    )
+  return (
+    <div className="card h-100">
+      <img src={img_url} className="card-img-top" alt={localita} />
+      <div className="card-body">
+        <h5 className="card-title">{localita}</h5>
+        <p className="card-text">{descrizione}</p>
+        <Link to={`travels/${id}`}>
+          <button className="btn btn-primary m-2">Dettagli Viaggio</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default TravelCard
+export default TravelCard;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,11 +5,11 @@ import travels_db from "../data/travels_db";
 function HomePage() {
   return (
     <>
-      <h1 className="mt-3 container">Lista viaggi attivi</h1>
       <div className="container ">
+        <h1 className="my-4 ">Lista viaggi attivi</h1>
         <div className="row g-4">
           {travels_db.map((travel) => (
-            <div key={travel.id} className="card col-12 col-md-6 col-lg-4">
+            <div key={travel.id} className=" col-12 col-md-6 col-lg-4">
               <TravelCard travelProp={travel} />
             </div>
           ))}
