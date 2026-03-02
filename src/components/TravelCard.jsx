8b3fe46b6e78ab7a77f75b2faabd8fe0ecbdadea
Pietro Scurti <pietroscurti9@gmail.com>
2026-03-02 13:37:13 +0100

    link su intera card

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -6,20 +6,20 @@ function TravelCard(props) {
   const { id, img_url, localita, descrizione } = props.travelProp;
 
   return (
-    <div className="card h-100">
-      <img
-        src={img_url}
-        className="card-img-top object-fit-cover fixed-height"
-        alt={localita}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{localita}</h5>
-        <p className="card-text">{descrizione}</p>
+    <Link to={`travels/${id}`}>
+      <div className="card h-100">
+        <img
+          src={img_url}
+          className="card-img-top object-fit-cover fixed-height"
+          alt={localita}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{localita}</h5>
+          <p className="card-text">{descrizione}</p>
+        </div>
+        {/* <span className="ps-3"><i>Dettagli Viaggio</i></span> */}
       </div>
-      <Link to={`travels/${id}`}>
-        <span className="ps-3"><i>Dettagli Viaggio</i></span>
-      </Link>
-    </div>
+    </Link>
   );
 }
 
