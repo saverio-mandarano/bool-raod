bd505effc15aa56efad749e8303ebd4902c15eb1
saverio mandarano <saverio.mandarano1@gmail.com>
2026-03-02 17:42:37 +0100

    add css per background navbar

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,23 +2,24 @@ import { Link } from "react-router-dom";
 import headerImg from "../assets/pngegg.png";
 
 function MainHeader() {
-    return (
-        <>
-            <header className=" mb-3 d-flex justify-content-between header-settings">
-                <div>
-                    <img src={headerImg} alt="" className="header-logo" />
-                </div>
+  return (
+    <>
+      <header className=" mb-3 d-flex justify-content-between header-settings">
+        <div>
+          <img src={headerImg} alt="" className="header-logo" />
+        </div>
 
-
-                <div className="button-margin">
-                    <Link to={`/`}>
-                        <span className="text-dark"> <i> Homepage </i> </span>
-                    </Link>
-                </div>
-
-            </header>
-        </>
-    );
+        <div className="button-margin">
+          <Link to={`/`}>
+            <span className="text-dark">
+              {" "}
+              <i className="link"> Homepage </i>{" "}
+            </span>
+          </Link>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default MainHeader;
